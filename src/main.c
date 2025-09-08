0777b5b4693d3d4bbcf088cd04ffbe1d9900e137
quantiom <35455461+quantiom@users.noreply.github.com>
2025-09-08 16:56:42 -0400

    Add starter files

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include "lexer.h"
+#include <stdio.h>
+
+int main()
+{
+	while (1) {
+		printf("> ");
+
+		/* input contains the whole command
+		 * tokens contains substrings from input split by spaces
+		 */
+
+		char *input = get_input();
+		printf("whole input: %s\n", input);
+
+		tokenlist *tokens = get_tokens(input);
+		for (int i = 0; i < tokens->size; i++) {
+			printf("token %d: (%s)\n", i, tokens->items[i]);
+		}
+
+		free(input);
+		free_tokens(tokens);
+	}
+
+	return 0;
+}
